e2f8d5733bb3ca099f98185a96704668e2cb4a09
mihir69 <vaghelamihir668@gmail.com>
2022-05-03 11:25:45 +0530

    initial commit

---
+++ client/src/components/Success.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+    <div className="card animated bounceIn">
+      <div className="card-body text-center pt-5 pb-5">
+          <i className="fas fa-check-circle fa-7x text-success"></i>
+          <h2>  your Resume almost ready</h2>
+          <br/>
+      </div>  
+    </div>
+  )
+}
+export default Success;
